cace33ef5c864cc28bf6f883113eef19657d6c2b
arttturslv <arttturslv@gmail.com>
2025-09-07 12:06:33 -0300

    fix: change browserRouter to hashRouter (github pages)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import Projects from "./pages/projects/index.tsx";
 import Doodles from "./pages/doodles/index.tsx";
 import Home from "./pages/home/index.tsx";
@@ -11,11 +11,11 @@ const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/doodles" element={<Doodles />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
